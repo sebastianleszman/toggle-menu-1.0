ddb2064d8d5cfd0031c6b9a4484d71f16e0fcb80
Sebastian Leszman <sebastianleszman@gmail.com>
2020-03-19 12:12:49 +0100

    another commit

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const hamburger = document.querySelector('.hamburger');
-
-const menuMain = document.querySelector('.menu__main');
+//hamburger effect
+const hamburger = document.querySelector(".hamburger");
+const menuMain = document.querySelector(".menu__main");
 
 const handleClick = () => {
-  hamburger.classList.toggle('hamburger--active');
-  menuMain.classList.toggle('menu__main--active');
+  hamburger.classList.toggle("hamburger--active");
+  menuMain.classList.toggle("menu__main--active");
 };
 
-hamburger.addEventListener('click', handleClick);
+hamburger.addEventListener("click", handleClick);
+
+//dropdown effect
+const dropdown = document.getElementsByClassName("dropdown");
+
+let i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("dropdown--active");
+    let dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
